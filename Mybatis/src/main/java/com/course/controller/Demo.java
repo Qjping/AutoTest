473a7142913879d076afa19d3d81ef2e10b40889
Qjping <37387574+Qjping@users.noreply.github.com>
2019-01-06 19:52:12 +0800

    mybatis的第一个demo

--- Mybatis/src/main/java/com/course/controller/Demo.java
+++ Mybatis/src/main/java/com/course/controller/Demo.java
@@ -1,11 +1,10 @@
 package com.course.controller;
 
+import com.course.model.User;
 import io.swagger.annotations.Api;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api(value="v1",description = "这是第一个版本的demo")
@@ -16,6 +15,23 @@ public class Demo {
     @GetMapping(value = "/getUserCount")
     public  int getUserCount(){
         return template.selectOne("getUserCount");
+    }
+    @PostMapping(value="/addUser")
+    public int postAddUser(@RequestBody User user){
+        return template.insert("addUser",user);
+    }
+
+    @PostMapping(value = "/updateUser")
+    public int updateUser(@RequestBody User user){
+        return template.update("updateUser",user);
+    }
+
+    @GetMapping(value = "/delUser/{id}")
+    public int delUser(@RequestParam int id){
+        if(id<0){
+            return -1;
+        }
+        return  template.delete("delUser",id);
     }
 }
 
---
+++ Mybatis/src/main/java/com/course/model/User.java
@@ -0,0 +1,11 @@
+package com.course.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    public int id;
+    public int age;
+    public String name;
+    public String sex;
+}
