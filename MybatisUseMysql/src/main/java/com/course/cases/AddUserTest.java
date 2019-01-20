55f92417c8783c2a2e0643749049a3aa4838b609
Qjping <37387574+Qjping@users.noreply.github.com>
2019-01-20 19:58:02 +0800

    接口自动化

--- MybatisUseMysql/src/main/java/com/course/cases/AddUserTest.java
+++ MybatisUseMysql/src/main/java/com/course/cases/AddUserTest.java
@@ -33,7 +33,7 @@ public class AddUserTest {
          * 可以先讲
          */
         //查询用户看是否添加成功
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         User user = session.selectOne("addUser",addUserCase);
         System.out.println(user.toString());
 
